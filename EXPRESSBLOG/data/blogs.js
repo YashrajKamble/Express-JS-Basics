3ab8956fd6c6207f4fc7301ff30888bad6d9a98f
Yashraj_K <yashrajkamble7727@gmail.com>
2026-02-12 20:07:37 +0530

    Added Routes with params

---
+++ EXPRESSBLOG/data/blogs.js
@@ -0,0 +1,31 @@
+
+blogs = [
+    {
+        title: "python",
+        content: "python content",
+        slug: "python-learning"
+    },
+    {
+        title: "react",
+        content: "react content",
+        slug: "react-learning"
+    },
+    {
+        title: "js",
+        content: "js content",
+        slug: "js-learning"
+    },
+    {
+        title: "ts",
+        content: "ts content",
+        slug: "ts content-learning"
+    },
+    {
+        title: "nodejs",
+        content: "nodejs content",
+        slug: "nodejs-learning"
+    },
+]
+
+
+module.exports = blogs
